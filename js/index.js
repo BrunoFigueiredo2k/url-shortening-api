6d84b9a7c60f6de261395d6c526f4811d1496f30
@BrunoFigueiredo2k <brunofigueiredo@live.nl>
2020-12-12 21:25:49 +0100

    api js stuff added only mobile styling left

--- js/index.js
+++ js/index.js
@@ -8,9 +8,10 @@ const submitBtn = document.getElementById('btn-shorten');
 submitBtn.addEventListener('click', () =>{
     // If input validation returns true continue
     if (validateInput()){
-        console.log(true);
+        // API GET request (getting short url)
+        getShortUrl();
     } else {
-        console.log(false);
+        console.log('Input validation status: ' + false);
     }
 })
 
@@ -25,4 +26,77 @@ function validateInput(){
         errorMsg.style.display = 'none';
         return true;
     }
+}
+
+/* API call */ 
+function getShortUrl(){
+    const baseUrl = "https://api.shrtco.de/v2/shorten?url=";
+
+    var request = new XMLHttpRequest();
+
+    request.open('GET', `${baseUrl + input.value}`, true);
+    request.onload = function () {
+        // Begin accessing JSON data here
+        var data = JSON.parse(this.response);
+
+        if (request.status >= 200 && request.status < 400) {
+            var shortLink = data.result.full_short_link;
+            console.log(shortLink)
+            console.log(data.result.full_short_link);
+        } else {
+            console.log('error');
+        }
+
+        // Call function to display shortlink to user
+        displayShortLink(shortLink);
+    }
+    request.send();
+}
+
+
+function displayShortLink(shortLink){
+    const linkContainer = document.querySelector('#urls-container');
+    const clearBtn = document.querySelector('#clear-urls');
+    const copiedTxt = 'Copied!';
+    const btnActiveClass = 'btn-copy-active';
+
+    linkContainer.innerHTML += `
+        <div class="card-url">
+            <p style="margin: 0;"><span class="input-url">${input.value}</span><button class="btn-copy">Copy</button><a
+                href="shortLink" target="_blank" class="short-url">${shortLink}</a></p>
+        </div>
+    `;
+    clearBtn.style.display = 'inline-block';
+    clickListenerClearList(clearBtn, linkContainer);
+
+    const copyBtns = document.querySelectorAll('.btn-copy');
+
+    copyBtns.forEach(element => {
+        element.addEventListener('click', () => {
+            console.log('clicked')
+            element.innerHTML = copiedTxt;
+            element.classList.add(btnActiveClass);
+            copyToClipboard(shortLink);
+        })
+    });
+}
+
+// Copy to clipboard function. Adds dummy textarea to body with link value so it can be copied
+function copyToClipboard(link){
+    var dummy = document.createElement("textarea");
+    document.body.appendChild(dummy);
+    dummy.value = link;
+    dummy.select();
+    document.execCommand("copy");
+
+    // Remove dummy textarea after copied
+    document.body.removeChild(dummy);
+    dummy.style.display = 'none'
+}
+
+function clickListenerClearList(button, clearContainer){
+    button.addEventListener('click', () => {
+        clearContainer.innerHTML = ``;
+        button.style.display = 'none';
+    })
 }
