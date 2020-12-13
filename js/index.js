df58eb4bf0df3a645d2d13730d713dc8b2e9a589
@BrunoFigueiredo2k <brunofigueiredo@live.nl>
2020-12-13 01:34:29 +0100

    mobile finished except url card :(

--- js/index.js
+++ js/index.js
@@ -97,3 +97,44 @@ function clickListenerClearList(button, clearContainer){
         button.style.display = 'none';
     })
 }
+
+/* Mobile menu */
+const CLOSE_ICON_SRC = "/images/icon-close.svg";
+const HAMBURGER_ICON_SRC = "/images/icon-hamburger.svg";
+const mobileMenu = document.querySelector('#mobile-menu-li');
+
+const mobileBtn = document.querySelector('#mobile-btn');
+var btnSrc = document.getElementById('mobile-btn').src;
+
+// Click listener hamburger menu btn
+mobileBtn.addEventListener('click', () => {
+    if (btnSrc == HAMBURGER_ICON_SRC){
+        mobileMenuActive(true);
+        mobileMenu.style.transform = 'scaleY(1)';
+
+        // Onclick outside hide again
+        event.stopPropagation();
+        window.onclick = function(e) {
+            if(e.target != mobileMenu) {
+                mobileMenuActive(false);
+                mobileMenu.style.transform = 'scaleY(0)';
+            } else {
+                mobileMenu.style.transform = 'scaleY(1)';
+            }
+        }    
+    } else {
+        mobileMenuActive(false);
+        mobileMenu.style.transform = 'scaleY(0)';
+    }
+});
+
+// Toggle source of mobile menu image btn
+function mobileMenuActive(active){
+    if (active){
+        btnSrc = CLOSE_ICON_SRC;
+        mobileBtn.setAttribute("src", CLOSE_ICON_SRC);
+    } else {
+        btnSrc = HAMBURGER_ICON_SRC;
+        mobileBtn.setAttribute("src", HAMBURGER_ICON_SRC);
+    }
+}
